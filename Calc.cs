2904e4a7f64e303509dac840060fcdb147928ff7
Alina20IT-2 <114774477+Alina20IT-2@users.noreply.github.com>
2022-11-05 00:03:56 +0300

    Add files via upload

---
+++ Calc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Калькулятор
+{
+    public class Calc
+    {
+        public double a = 0;
+
+        public void Put_A(double a)
+        {
+            this.a = a;
+        }
+
+        public double Sum(double b)
+        {
+            return a + b;
+        }
+
+        public double Min(double b)
+        {
+            return a - b;
+        }
+
+        public double Mult(double b)
+        {
+            return a * b;
+        }
+
+        public double Division(double b)
+        {
+            return a / b;
+        }
+
+        public void Clear()
+        {
+            a = 0;
+        }
+    }
+}
